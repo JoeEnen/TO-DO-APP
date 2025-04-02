04c153262cedba96710eed623e02979f9bf1695d
JoeEnen <enenjoseph2@gmail.com>
2025-04-02 15:25:26 +0300

    new commit

--- prisma/migrations/20250330184143_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-
-    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
-);
